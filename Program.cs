c8b7e1c7c7cb31a55ec52cec50b044dbd2e3575d
dodrui <Prostopochta316@gmail.com>
2024-03-19 19:45:44 +0500

    sth

---
+++ Program.cs
@@ -0,0 +1,269 @@
+﻿using Laba10;
+using System.Diagnostics;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Diagnostics.Metrics;
+
+namespace Laba11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Задание 1
+            //1.1
+
+            ArrayList al  = new ArrayList();
+            for (int i = 0; i < 5; i++)
+            {
+                Jet j = new Jet();
+                al.Add(j);
+                j.RandomInit();
+            }
+            for (int i = 0; i < 5; i++)
+            {
+                Helecopter j = new Helecopter();
+                al.Add(j);
+                j.RandomInit();
+            }
+
+            Console.WriteLine("Введите номер объекта:");
+            Aircraft airSearch = new Aircraft();
+            airSearch.Init();
+
+            int position = al.IndexOf(airSearch);
+
+            Console.WriteLine($"Искомая позиция: {position}");
+            if (position >= 0)
+            {
+                Console.WriteLine($"Удаляем {al[position]}");
+                al.Remove(position);
+            }
+
+            if (al.Contains(position))
+            {
+                Console.WriteLine("Найден");
+            }
+            else
+            {
+                Console.WriteLine("Не найден");
+            }
+
+            //Сортировка по типу двигателя
+            al.Sort();
+            ShowArray(al);
+            Console.WriteLine(al.Count);
+            Console.WriteLine(al.Capacity);
+
+            Console.WriteLine($"Суммарное колличество экипажа истребителей типа перехватчик: {CrewCount(al)}");
+            Console.WriteLine($"Количество самолетов произведенных с 2010 года = {YearCount(al)}");
+            Console.WriteLine($"Количество вертолетов: {HelecopterCount(al)}");
+
+            Console.WriteLine("Спиок истребителей:");
+            foreach (object j in al)
+            {
+                if (j is Jet p)
+                {
+                    Console.WriteLine(p);
+                }
+            }
+
+            ArrayList alClon = ToClone(al);
+            Console.WriteLine("Клон:");
+            ShowArray(alClon);
+
+            //2 задание
+            Queue<Aircraft> queue = new Queue<Aircraft>();
+            for (int i = 0; i < 5; i++)
+            {
+                Jet j = new Jet();
+                j.RandomInit();
+                queue.Enqueue(j);
+            }
+            for (int i = 0; i < 5; i++)
+            {
+                Helecopter j = new Helecopter();
+                j.RandomInit();
+                queue.Enqueue(j);
+            }
+            ShowQueue(queue);
+
+            //Поиск
+            Console.WriteLine("Введите элемент для поиска:");
+            Aircraft forSearch = new Aircraft();
+            forSearch.Init();
+
+            if (queue.Contains(forSearch))
+                Console.WriteLine("Элемент найден");
+            else
+                Console.WriteLine("Элемент не найден");
+
+            //Удаление
+            Console.WriteLine("Введите элемент для удаления:");
+            Aircraft forDel = new Aircraft();
+            forDel.Init();
+            queue = RemoveFromQueue(queue, forDel);
+            Console.WriteLine("Элемент удален!");
+            ShowQueue(queue);
+
+            //Запросы
+            Console.WriteLine($"Истребителей в очереди: {JetCount(queue)}");
+            Console.WriteLine($"Самолетов в очереди: {PlaneCount(queue)}");
+            Console.WriteLine($"Самолетов с вместимостью от 200 человек: {PassengersCount(queue)}");
+
+            //Сортировка
+            queue = SortQueue(queue);
+            Console.WriteLine("Обновленная очередь:");
+            ShowQueue(queue);
+
+            //Клонирование
+            Queue<Aircraft> queueClon = ToCloneQueue(queue);
+            Console.WriteLine("Клон:");
+            ShowQueue(queueClon);
+
+            //3
+            TestCollections collections = new TestCollections(10);
+            Console.WriteLine("тестовые коллекции созданы");
+            Console.WriteLine(collections.FirstElem);
+            Console.WriteLine("\n несколько раз запущенный поиск первого элемента");
+            Console.WriteLine(collections.FirstEl());
+            Console.WriteLine(collections.FirstEl());
+            Console.WriteLine(collections.FirstEl());
+
+            Console.WriteLine("\n несколько раз запущенный поиск срединного элемента");
+            Console.WriteLine(collections.MiddleEl());
+            Console.WriteLine(collections.MiddleEl());
+
+            Console.WriteLine("\n несколько раз запущенный поиск последнего элемента");
+            Console.WriteLine(collections.LastEl());
+            Console.WriteLine(collections.LastEl());
+
+            Console.WriteLine("\n несколько раз запущенный поиск несуществующего элемента");
+            Console.WriteLine(collections.NotExistEl());
+            Console.WriteLine(collections.NotExistEl());
+        }
+        static void ShowArray (ArrayList al)
+        {
+            foreach (object o in al)
+            {
+                Console.WriteLine(o.ToString());
+            }
+        }
+        static int CrewCount(ArrayList al)
+        {
+            int cnt = 0;
+            string type = "перехватчик";
+            foreach (object item in al)
+            {
+                if (item is Jet j)
+                {
+                    if (j.Function == type)
+                        cnt += j.CrewNumber;
+                }
+            }
+            return cnt;
+        }
+        static int YearCount(ArrayList al)
+        {
+            int cnt = 0;
+            foreach (object item in al)
+            {
+                Aircraft air = item as Aircraft;
+                if (air.ProductionYear >= 2010 && (item != null))
+                    cnt++;
+            }
+            return cnt;
+        }
+        static int HelecopterCount(ArrayList al)
+        {
+            int cnt = 0;
+            foreach(Aircraft item in al)
+            {
+                if (item is Helecopter j)
+                    cnt++;
+            }
+            return cnt;
+        }
+        public static ArrayList ToClone(ArrayList al)
+        {
+            ArrayList alClone = new ArrayList();
+            foreach (object item in al)
+            {
+                alClone.Add(((ICloneable)item).Clone());
+            }
+            return alClone;
+        }
+        public static void ShowQueue <T> (Queue <T> queue)
+        {
+            foreach (T item in queue)
+                Console.WriteLine(item);
+        }
+        public static Queue<T> ToCloneQueue<T>(Queue<T> queue)
+        {
+            Queue<T> clone = new Queue<T>(queue.ToArray());
+            return clone;
+        }
+        public static Queue<T> RemoveFromQueue<T> (Queue<T> queue, Aircraft forDel) 
+        { 
+            Queue<T> newArr = new Queue<T>();
+            while (queue.Count > 0)
+            {
+                T temp = queue.Dequeue();
+                if (!temp.Equals(forDel))
+                {
+                    newArr.Enqueue(temp);
+                }
+                else
+                {
+                    Console.WriteLine("Объект удален");
+                }
+            }
+            return newArr;
+        }
+        public static Queue<T> SortQueue<T> (Queue<T> queue)
+        {
+            T[] arrayed = queue.ToArray();
+            Array.Sort(arrayed);
+
+            queue.Clear();
+            foreach (T item in arrayed)
+                queue.Enqueue(item);
+            return queue;
+        }
+        public static int JetCount<T>(Queue<T> queue)
+        {
+            int cnt = 0;
+            foreach (object item in queue)
+            {
+                if (item is Jet) 
+                    cnt++;
+            }
+            return cnt;
+        }
+
+        public static int PlaneCount<T>(Queue<T> queue)
+        {
+            int cnt = 0;
+            foreach (object item in queue)
+            {
+                if (item is Plane)
+                    cnt++;
+            }
+            return cnt;
+        }
+        public static int PassengersCount<T>(Queue<T> queue)
+        {
+            int cnt = 0;
+            foreach (object item in queue)
+            {
+                if (item is Plane p)
+                {
+                    if (p.Passengers > 200)
+                        cnt++;
+                }
+            }
+            return (cnt);
+        }
+    }
+}
---
+++ TestCollections.cs
@@ -0,0 +1,169 @@
+﻿using Laba10;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laba11
+{
+    public class TestCollections
+    {
+        SortedDictionary<Aircraft, Jet> col1 = new SortedDictionary<Aircraft, Jet>();
+        SortedDictionary<String, Jet> col2 = new SortedDictionary<String, Jet>();
+
+        HashSet<Jet> col3 = new HashSet<Jet>();
+        HashSet<String> col4 = new HashSet<String>();
+
+
+        //Конструктор для заполнения коллекция 1000 штук элементов.
+        Jet firstElem = new Jet();
+        Jet middleElem = new Jet();
+        Jet lastElem = new Jet();
+
+        public TestCollections(int len)
+        {
+            Jet jj = new Jet();
+            jj.RandomInit();
+            for (int i = 0; i < len; i++)
+            {
+                try
+                {
+                    Jet jet = new Jet();
+                    jet.RandomInit();
+                    //Добавим цифру для уникальности имени
+                    jet.Model += i.ToString();
+
+                    //Добавление элементов
+                    col1.Add(jet.GetBase, jet);
+                    col2.Add(jet.GetBase.ToString(), jet);
+                    col3.Add(jet);
+                    col4.Add(jet.ToString());
+
+                    //Поиск
+                    if (i == 0)
+                    {
+                        firstElem = (Jet)jet.Clone();
+                    }
+                    else if (i == len / 2)
+                    {
+                        middleElem = (Jet)jet.Clone();
+                    }
+                    else if (i == len - 1)
+                    {
+                        lastElem = (Jet)jet.Clone();
+                    }
+
+                    Console.WriteLine(jet);
+                }
+                catch
+                {
+                    Console.WriteLine("2");
+                    i--;
+                }
+            }
+        }
+        public Jet FirstElem
+        {
+            get => firstElem;
+        }
+        public string FirstEl()
+        {
+            Stopwatch sw = new Stopwatch();
+            string output = "";
+
+            sw.Start();
+            bool res = col1.ContainsValue(firstElem);
+            sw.Stop();
+            output += $"Средний элемент в SortedDictionary<Aircraft, Jet> " + (res ? "найден" : "не найден") + $" за {sw.ElapsedTicks}\n";
+            sw.Restart();
+            res = col2.ContainsValue(firstElem);
+            sw.Stop();
+            output += $"Средний элемент в SortedDictionary<String, Jet> " + (res ? "найден" : "не найден") + $" за {sw.ElapsedTicks}\n";
+            sw.Restart();
+            res = col3.Contains(firstElem);
+            sw.Stop();
+            output += $"Средний элемент в HashSet<Jet> " + (res ? "найден" : "не найден") + $" за {sw.ElapsedTicks}\n";
+            sw.Restart();
+            res = col4.Contains(firstElem.ToString());
+            sw.Stop();
+            output += $"Средний элемент в HashSet<String> " + (res ? "найден" : "не найден") + $" за {sw.ElapsedTicks}\n";
+
+            return output;
+        }
+        public string MiddleEl()
+        {
+            Stopwatch sw = new Stopwatch();
+            string output = "";
+
+            sw.Start();
+            bool res = col1.ContainsValue(middleElem);
+            sw.Stop();
+            output += $"Последний элемент в SortedDictionary<Aircraft, Jet> " + (res ? "найден" : "не найден") + $" за {sw.ElapsedTicks}\n";
+            sw.Restart();
+            res = col2.ContainsValue(middleElem);
+            sw.Stop();
+            output += $"Последний элемент в SortedDictionary<String, Jet> " + (res ? "найден" : "не найден") + $" за {sw.ElapsedTicks}\n";
+            sw.Restart();
+            res = col3.Contains(middleElem);
+            sw.Stop();
+            output += $"Последний элемент в HashSet<Jet> " + (res ? "найден" : "не найден") + $" за {sw.ElapsedTicks}\n";
+            sw.Restart();
+            res = col4.Contains(middleElem.ToString());
+            sw.Stop();
+            output += $"Последний элемент в HashSet<String> " + (res ? "найден" : "не найден") + $" за {sw.ElapsedTicks}\n";
+
+            return output;
+        }
+        public string LastEl()
+        {
+            Stopwatch sw = new Stopwatch();
+            string output = "";
+
+            sw.Start();
+            bool res = col1.ContainsValue(lastElem);
+            sw.Stop();
+            output += $"Первый элемент в SortedDictionary<Aircraft, Jet> " + (res ? "найден" : "не найден") + $" за {sw.ElapsedTicks}\n";
+            sw.Restart();
+            res = col2.ContainsValue(lastElem);
+            sw.Stop();
+            output += $"Первый элемент в SortedDictionary<String, Jet> " + (res ? "найден" : "не найден") + $" за {sw.ElapsedTicks}\n";
+            sw.Restart();
+            res = col3.Contains(lastElem);
+            sw.Stop();
+            output += $"Первый элемент в HashSet<Jet> " + (res ? "найден" : "не найден") + $" за {sw.ElapsedTicks}\n";
+            sw.Restart();
+            res = col4.Contains(lastElem.ToString());
+            sw.Stop();
+            output += $"Первый элемент в HashSet<String> " + (res ? "найден" : "не найден") + $" за {sw.ElapsedTicks}\n";
+
+            return output;
+        }
+        public string NotExistEl()
+        {
+            Jet jet = new Jet("None", -1, "None", 5, 0,"None");
+            Stopwatch sw = new Stopwatch();
+            string output = "";
+
+            sw.Start();
+            bool res = col1.ContainsValue(jet);
+            sw.Stop();
+            output += $"Первый элемент в SortedDictionary<Aircraft, Jet> " + (res ? "найден" : "не найден") + $" за {sw.ElapsedTicks}\n";
+            sw.Restart();
+            res = col2.ContainsValue(jet);
+            sw.Stop();
+            output += $"Первый элемент в SortedDictionary<String, Jet> " + (res ? "найден" : "не найден") + $" за {sw.ElapsedTicks}\n";
+            sw.Restart();
+            res = col3.Contains(jet);
+            sw.Stop();
+            output += $"Первый элемент в HashSet<Jet> " + (res ? "найден" : "не найден") + $" за {sw.ElapsedTicks}\n";
+            sw.Restart();
+            res = col4.Contains(jet.ToString());
+            sw.Stop();
+            output += $"Первый элемент в HashSet<String> " + (res ? "найден" : "не найден") + $" за {sw.ElapsedTicks}\n";
+
+            return output;
+        }
+    }
+}
